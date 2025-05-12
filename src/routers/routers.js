e30132d84912bec26d2f41b81cd9a4c19a5ac018
Beatriz Rizzo <coding.bea@gmail.com>
2025-05-11 22:51:00 -0300

    correção da routers

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,8 +1,25 @@
+// routers.js
+
 const express = require('express');
 const router = express.Router();
-const usersController = require('../controllers/usersController'); 
+const studentsControllers = require('../controllers/studentsControllers');
+const teachersControllers = require('../controllers/teachersControllers');
 
-router.get('/users', usersController.listAll);
-router.get('/user/:id', usersController.listOne);
+// Rotas para alunos
+router.get('/students', studentsControllers.listAll);
+router.get('/student/:ra', studentsControllers.getOne);
+router.post('/student', studentsControllers.create);
+router.put('/student/:ra', studentsControllers.update);
+router.delete('/student/:ra', studentsControllers.remove);
+
+// Rotas para professores
+router.get('/teachers', teachersControllers.listAll);
+router.get('/teacher/:rm', teachersControllers.getOne);
+router.post('/teacher', teachersControllers.create);
+router.put('/teacher/:rm', teachersControllers.update);
+router.delete('/teacher/:rm', teachersControllers.remove);
+
+// Rota para login
+// router.post('/login', loginControllers.login);
 
 module.exports = router;
